f0dfb43c638de086e647c2f757647a6e923a0825
GitHub Action <action@github.com>
2025-05-11 13:29:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexandra7a/pluginlicenta/MyBundle.kt
+++ src/main/kotlin/com/github/alexandra7a/pluginlicenta/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alexandra7a.pluginlicenta
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alexandra7a/pluginlicenta/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alexandra7a/pluginlicenta/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexandra7a.pluginlicenta.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexandra7a.pluginlicenta.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
